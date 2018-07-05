c3586d0a0c4ee023fd827646ec6ffd92c883c053
Evgeniy Voloshyn <e.voloshyn@spd-ukraine.com>
2018-07-05 19:54:03 +0300

    add calculator

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package calculator;
+
+public class Calculator {
+
+    public int add(int first, int second){
+        return first + second;
+    }
+
+    public int substract(int minuend, int subtrahend){
+        return minuend - subtrahend;
+    }
+
+    public int multiply(int first, int second){
+        return first * second;
+    }
+
+    public int divide(int dividend, int divisor){
+        return dividend/divisor;
+    }
+}
