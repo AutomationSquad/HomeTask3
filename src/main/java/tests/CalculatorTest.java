f833f9c72e261cd3fc12a01d6382aed9b8c78b5e
vitaliisotnichenko <vitalii.sotnichenko@gmail.com>
2018-07-10 22:36:32 +0300

    task3-homework-fixed

--- src/main/java/tests/CalculatorTest.java
+++ src/main/java/tests/CalculatorTest.java
@@ -1,54 +1,66 @@
 package tests;
 
 import calculator.Calculator;
+import calculator.Listeners;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-public class CalculatorTest {
+
+
+public class CalculatorTest extends Listeners {
 
   Calculator c = new Calculator();
-
-
-  long startTime = System.currentTimeMillis();
-
 
 
   @Test
 
   public void testAdd() {
 
+    long startTime = System.currentTimeMillis();
     Assert.assertEquals("the result adding is false", 6, c.add(2, 4));
     Assert.assertEquals("the result adding is false", 11, c.add(3, 8));
     Assert.assertEquals("the result adding is false", 61, c.add(50, 11));
     Assert.assertEquals("the result adding is false", 44, c.add(42, 2));
     Assert.assertEquals("the result adding is false", 14, c.add(2, 12));
 
-
+    long endTime = System.currentTimeMillis();
+    long diff = endTime - startTime;
+    System.out.println("The tests run " + diff + " mc");
   }
 
   @Test
 
   public void testSubtract() {
 
+
+    long startTime = System.currentTimeMillis();
     Assert.assertEquals("the result adding is false", -2, c.substract(2, 4));
     Assert.assertEquals("the result adding is false", -5, c.substract(3, 8));
     Assert.assertEquals("the result adding is false", 39, c.substract(50, 11));
     Assert.assertEquals("the result adding is false", 40, c.substract(42, 2));
     Assert.assertEquals("the result adding is false", -10, c.substract(2, 12));
 
+    long endTime = System.currentTimeMillis();
+    long diff = endTime - startTime;
+    System.out.println("The tests run " + diff + " mc");
 
   }
+
 
   @Test
 
   public void testmultiply() {
 
+    long startTime = System.currentTimeMillis();
     Assert.assertEquals("the result adding is false", 8, c.multiply(2, 4));
     Assert.assertEquals("the result adding is false", 24, c.multiply(3, 8));
     Assert.assertEquals("the result adding is false", 550, c.multiply(50, 11));
     Assert.assertEquals("the result adding is false", 84, c.multiply(42, 2));
     Assert.assertEquals("the result adding is false", 24, c.multiply(2, 12));
 
+    long endTime = System.currentTimeMillis();
+    long diff = endTime - startTime;
+    System.out.println("The tests run " + diff + " mc");
 
   }
 
@@ -56,27 +68,38 @@ public class CalculatorTest {
 
   public void testDivide() {
 
+    long startTime = System.currentTimeMillis();
     Assert.assertEquals("the result adding is false", 2, c.divide(4, 2));
     Assert.assertEquals("the result adding is false", 2, c.divide(8, 3));
     Assert.assertEquals("the result adding is false", 4, c.divide(50, 11));
     Assert.assertEquals("the result adding is false", 21, c.divide(42, 2));
     Assert.assertEquals("the result adding is false", 6, c.divide(12, 2));
 
-
+    long endTime = System.currentTimeMillis();
+    long diff = endTime - startTime;
+    System.out.println("The tests run " + diff + " mc");
   }
+
 
 
   @Test
 
   public void testNotNull() {
 
+    long startTime = System.currentTimeMillis();
     Assert.assertNotNull("Not null digit", c);
+
+    long endTime = System.currentTimeMillis();
+    long diff = endTime - startTime;
+    System.out.println("The tests run " + diff + " mc");
 
 
   }
 
   @Test
   void testDivideZero() {
+
+    long startTime = System.currentTimeMillis();
 
     try {
 
@@ -88,11 +111,11 @@ public class CalculatorTest {
 
     }
 
-
     long endTime = System.currentTimeMillis();
     long diff = endTime - startTime;
     System.out.println("The tests run " + diff + " mc");
-  }
 
+
+  }
 
 }
