0f46027fab16775d77acf601ac7959a2eab669f9
Valieva Venera <valievavm@gmail.com>
2018-07-11 11:02:29 +0300

    Parameterized tests for Calculator with Listener

---
+++ src/main/java/tests/TestListener.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class TestListener implements AfterEachCallback, BeforeEachCallback {
+
+    public long startRunTime, finishRunTime, testRunTime;
+
+    @Override
+    public void beforeEach(ExtensionContext extensionContext) throws Exception {
+        startRunTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        finishRunTime = System.currentTimeMillis();
+        testRunTime = finishRunTime - startRunTime;
+        System.out.println("Execution time is "  + testRunTime + " ms");
+    }
+}
+
+
